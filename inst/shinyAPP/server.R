a43b75e5a42796cb5785c5eaffcfe5a68afd2959
marzione00 <marziodecorato@cln-189-106.eduroam.unimi.it>
2020-02-12 09:08:31 +0100

    pub

--- inst/shinyAPP/server.R
+++ inst/shinyAPP/server.R
@@ -57,7 +57,7 @@ server <- function(input, output, session) {
 
   G_2<-reactive({input$"GR2"
 
-    granger<-coint.test(matrix[,G_1()], matrix[,G_2()], d = 0, nlag = NULL, output = TRUE) #granger causality test, to be done
+    coint.test(matrix[,G_1()], matrix[,G_2()], d = 0, nlag = NULL, output = TRUE) #granger causality test, to be done
 
   })
 
