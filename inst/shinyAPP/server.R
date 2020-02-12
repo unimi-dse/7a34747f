ab96ea5ab9004e1eba1f903cab2a2236a335604b
marzione00 <marziodecorato@cln-189-106.eduroam.unimi.it>
2020-02-12 09:48:58 +0100

    pub

--- inst/shinyAPP/server.R
+++ inst/shinyAPP/server.R
@@ -92,7 +92,7 @@ server <- function(input, output, session) {
 
   output$coolplot5<- renderText(
 
-    coint.test(matrix[,G_1()], matrix[,G_2()]) #granger causality test, to be done
+    aTSA::coint.test(matrix[,G_1()], matrix[,G_2()]) #granger causality test, to be done
 
   )
 
